0100c12082cb6d91759e8171efd41d3bc4e12556
Bernhard B <bernhard@liftingcoder.com>
2026-05-23 11:42:05 +0200

    extend clientquery.go to add support for json and timestamp values
    * do explicit conversion + get rid off reflection

--- sqlite3/clientquery.go
+++ sqlite3/clientquery.go
@@ -1,10 +1,9 @@
 package gluasql_sqlite3
 
 import (
-	"reflect"
+	"fmt"
+	"time"
 
-	util "github.com/bbernhard/gluasql/util"
-	"github.com/junhsieh/goexamples/fieldbinding/fieldbinding"
 	lua "github.com/yuin/gopher-lua"
 )
 
@@ -13,7 +12,9 @@ func clientQueryMethod(L *lua.LState) int {
 	query := L.ToString(2)
 
 	if client.DB == nil {
-		return 0
+		L.Push(lua.LNil)
+		L.Push(lua.LString("database not initialized"))
+		return 2
 	}
 
 	if query == "" {
@@ -29,7 +30,6 @@ func clientQueryMethod(L *lua.LState) int {
 	}
 	defer rows.Close()
 
-	fb := fieldbinding.NewFieldBinding()
 	cols, err := rows.Columns()
 	if err != nil {
 		L.Push(lua.LNil)
@@ -37,20 +37,101 @@ func clientQueryMethod(L *lua.LState) int {
 		return 2
 	}
 
-	fb.PutFields(cols)
+	resultTable := L.NewTable()
 
-	tb := L.NewTable()
 	for rows.Next() {
-		if err := rows.Scan(fb.GetFieldPtrArr()...); err != nil {
+		values := make([]interface{}, len(cols))
+		valuePtrs := make([]interface{}, len(cols))
+
+		for i := range values {
+			valuePtrs[i] = &values[i]
+		}
+
+		// Scan row values
+		if err := rows.Scan(valuePtrs...); err != nil {
 			L.Push(lua.LNil)
 			L.Push(lua.LString(err.Error()))
 			return 2
 		}
 
-		tbRow := util.ToTableFromMap(L, reflect.ValueOf(fb.GetFieldArr()))
-		tb.Append(tbRow)
+		// Convert row to Lua table
+		rowTable := L.NewTable()
+
+		for i, col := range cols {
+			val := values[i]
+
+			switch v := val.(type) {
+			case nil:
+				rowTable.RawSetString(col, lua.LNil)
+
+			case int:
+				rowTable.RawSetString(col, lua.LNumber(v))
+
+			case int8:
+				rowTable.RawSetString(col, lua.LNumber(v))
+
+			case int16:
+				rowTable.RawSetString(col, lua.LNumber(v))
+
+			case int32:
+				rowTable.RawSetString(col, lua.LNumber(v))
+
+			case int64:
+				rowTable.RawSetString(col, lua.LNumber(v))
+
+			case uint:
+				rowTable.RawSetString(col, lua.LNumber(v))
+
+			case uint8:
+				rowTable.RawSetString(col, lua.LNumber(v))
+
+			case uint16:
+				rowTable.RawSetString(col, lua.LNumber(v))
+
+			case uint32:
+				rowTable.RawSetString(col, lua.LNumber(v))
+
+			case uint64:
+				rowTable.RawSetString(col, lua.LNumber(v))
+
+			case float32:
+				rowTable.RawSetString(col, lua.LNumber(v))
+
+			case float64:
+				rowTable.RawSetString(col, lua.LNumber(v))
+
+			case bool:
+				rowTable.RawSetString(col, lua.LBool(v))
+
+			case string:
+				rowTable.RawSetString(col, lua.LString(v))
+
+			case []byte:
+				rowTable.RawSetString(col, lua.LString(string(v)))
+
+			case time.Time:
+				rowTable.RawSetString(
+					col,
+					lua.LString(v.Format(time.RFC3339)),
+				)
+
+			default:
+				rowTable.RawSetString(
+					col,
+					lua.LString(fmt.Sprint(v)),
+				)
+			}
+		}
+
+		resultTable.Append(rowTable)
 	}
 
-	L.Push(tb)
+	if err := rows.Err(); err != nil {
+		L.Push(lua.LNil)
+		L.Push(lua.LString(err.Error()))
+		return 2
+	}
+
+	L.Push(resultTable)
 	return 1
 }
--- sqlite3/clientquery_test.go
+++ sqlite3/clientquery_test.go
@@ -2,8 +2,7 @@ package gluasql_sqlite3_test
 
 import (
 	"testing"
-
-	"fmt"
+	"time"
 
 	"github.com/bbernhard/gluasql"
 	util "github.com/bbernhard/gluasql/util"
@@ -46,6 +45,52 @@ func TestClientQuery(t *testing.T) {
 			"phone_numbers": "[]",
 			"name":          "John Doe",
 			"created_at":    timeNowFormat,
+		},
+	}, res)
+}
+
+func TestClientQueryJsonAndTimestamp(t *testing.T) {
+	assert := assert.New(t)
+
+	L := lua.NewState()
+	defer L.Close()
+
+	gluasql.Preload(L)
+
+	timeNowFormat := timeNow.UTC().Format(time.RFC3339)
+
+	script := getLuaDbConnection() + `
+		c:exec([[
+			CREATE TABLE IF NOT EXISTS messages (
+				id INTEGER PRIMARY KEY,
+				data JSON,
+				timestamp DATETIME
+			);
+		]])
+
+		c:exec([[
+			INSERT INTO messages(data, timestamp)
+			VALUES(
+				'{"message":"hello","count":1}',
+				'` + timeNowFormat + `'
+			);
+		]])
+
+		res, err = c:query("SELECT data, timestamp FROM messages LIMIT 1");
+		return res, err;
+	`
+
+	assert.NoError(L.DoString(script))
+
+	res := util.GetValue(L, 1)
+	err := util.GetValue(L, 2)
+
+	assert.Nil(err)
+
+	assert.Equal([]interface{}{
+		map[string]interface{}{
+			"data":      `{"message":"hello","count":1}`,
+			"timestamp": timeNowFormat,
 		},
 	}, res)
 }
